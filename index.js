01f620c38ef4282312a06d5c4d1128454f1ff6db
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-05 21:39:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function getFirstSelector(selector){
+  document.querySelector(selector)[0]
+}
+
+function nestedTarget(){
+  getFirstSelector("#nested .target")
+}
+
+function increaseRankBy(){}
+
+function deepestChild(){}
